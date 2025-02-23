abe30132752c471405aec29347e9e38821de85cd
Nithinjs615 <nithinjs615@gmail.com>
2025-02-23 15:13:03 -0500

    implemented Addition.java

---
+++ GitTutorialProject/src/main/java/org/example/Addition.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a=10;
+        int b=15;
+        int sum=a+b;
+        System.out.println("sum is "+sum);
+    }
+}
